f14477ca482a33c56157d136b961b0bb552b0ddc
Diego Blanco <diego.blanco@treitos.com>
2019-12-16 23:19:28 +0100

    Improved the assignation of internal variables

--- lse.sh
+++ lse.sh
@@ -345,7 +345,7 @@ lse_test() {
       output="`eval "$cmd" 2>/dev/null`"
     # Assign variable if available
     fi
-    [ "$var" ] && eval "$var='$output'"
+    [ "$var" ] && export "${var}"="$output"
     # Mark test as executed
     lse_executed_tests+=" $id"
   fi
