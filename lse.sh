8bc19e8b689adec2faece478ff6347af75485d00
Diego Blanco <diego.blanco@treitos.com>
2020-03-31 11:21:35 +0200

    Saved the users with shell to a variable and a minor fix in the same test

--- lse.sh
+++ lse.sh
@@ -497,7 +497,9 @@ lse_run_tests_users() {
   #other users with shell
   lse_test "usr030" "1" \
     "Other users with shell" \
-    'grep -E "sh$" /etc/passwd'
+    'grep -E "sh\$" /etc/passwd'
+    "" \
+    "lse_shell_users"
     
   #user env information
   lse_test "usr040" "2" \
