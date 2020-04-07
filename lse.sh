b722bab46838810afe20e9a9f8dbe0f7841c2f21
Diego Blanco <diego.blanco@treitos.com>
2020-04-08 01:01:25 +0200

    Possible fix for #28

--- lse.sh
+++ lse.sh
@@ -390,7 +390,7 @@ lse_test() {
       output="`eval "$cmd" 2>/dev/null`"
     # Assign variable if available
     fi
-    [ "$var" ] && declare -g "${var}=$output"
+    [ "$var" ] && readonly "${var}=$output"
     # Mark test as executed
     lse_executed_tests+=" $id"
   fi
